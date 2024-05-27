63c54e075b85340cc3a781800ea11f37ee81c8cc
JonasKW <jonas.meza.143@gmail.com>
2024-05-27 11:48:10 -0600

    Se implemento la tabla para listar a los exhortos y el sweealert

--- src/app/Modulos/panel/exhortos-nacionales/exhortos-nacionales.component.ts
+++ src/app/Modulos/panel/exhortos-nacionales/exhortos-nacionales.component.ts
@@ -1,14 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, inject, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, Validators, FormBuilder, FormsModule } from '@angular/forms';
 import { NgZorroAntdModule } from 'src/app/shared/ng-zorro';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { TableModule } from 'primeng/table';
+import { Table } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { MenuItem, MessageService } from 'primeng/api';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { DialogModule } from 'primeng/dialog';
+import { ToastModule } from 'primeng/toast';
+import { DropdownModule } from 'primeng/dropdown';
+import { environment } from 'src/environments/environment'
+import { TabViewModule } from 'primeng/tabview';
+import { BadgeModule } from 'primeng/badge';
 
 @Component({
   selector: 'app-exhortos-nacionales',
   standalone: true,
-  imports: [CommonModule,NgZorroAntdModule],
   templateUrl: './exhortos-nacionales.component.html',
-  styleUrls: ['./exhortos-nacionales.component.scss']
+  styleUrls: ['./exhortos-nacionales.component.scss'],
+  imports: [
+    CommonModule,
+    NgZorroAntdModule,
+    ReactiveFormsModule,
+    FormsModule,
+    TableModule,
+    TagModule,
+    ToastModule,
+    SplitButtonModule,
+    DialogModule,
+    DropdownModule,
+    TabViewModule,
+    BadgeModule
+  ],
+  providers: [MessageService]
 })
-export class ExhortosNacionalesComponent {
+export class ExhortosNacionalesComponent implements OnInit {
+  @ViewChild('dt') dt: Table | undefined;
+  exhortos: any[] = [];
 
+
+  ngOnInit(): void {
+
+  }
+
+  SearchTable(event: any, stringVal: string) {
+    if (this.dt) {
+      const inputValue = (event.target as HTMLInputElement).value.toLowerCase(); // Convertir a minúsculas
+      //this.dt.filterGlobal(inputValue, 'contains');
+      this.dt.filterGlobal(inputValue, 'startsWith');
+    }
+  }
 }
